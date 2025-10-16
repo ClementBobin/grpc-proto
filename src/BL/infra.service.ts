f3252bc350517dd2643f15c5e1cb0ec078f1b4c2
Mirage <119869686+ClementBobin@users.noreply.github.com>
2025-10-16 06:31:09 +0000

    Feat: init

---
+++ src/BL/infra.service.ts
@@ -0,0 +1,8 @@
+import { InfraRepository } from '../DAL/infra.repository';
+
+export const InfraService = {
+  healthCheck: async () => {
+    // Could do DB check, cache check, etc.
+    return InfraRepository.getHealthStatus();
+  }
+};
