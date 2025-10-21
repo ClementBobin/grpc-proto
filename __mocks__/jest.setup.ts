6cf839eaaac65655d9ea50071afe40d20b2b987e
Mirage <119869686+ClementBobin@users.noreply.github.com>
2025-10-21 10:20:29 +0200

    Update logger mock to use logger.module
    Signed-off-by: Mirage <119869686+ClementBobin@users.noreply.github.com>

--- __mocks__/jest.setup.ts
+++ __mocks__/jest.setup.ts
@@ -1,7 +1,7 @@
 import { jest } from '@jest/globals';
 import { mockLogger } from './test-utils';
 
-jest.mock('../src/lib/modules/logger', () => ({
+jest.mock('../src/lib/modules/logger.module', () => ({
   __esModule: true,
   default: mockLogger(),
 }));
